492a15a43168acea530f7d7a29008f8242d2b3c7
Max Soe <maxsoe@gmail.com>
2019-01-25 16:29:32 +0400

    Updating the State 1:10:51

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -39,7 +39,7 @@ class Counter extends Component {
   }
 
   handleIncrement = () => {
-    console.log("increment clicked", this);
+    this.setState({ count: this.state.count + 1 });
   };
 
   renderTags() {
