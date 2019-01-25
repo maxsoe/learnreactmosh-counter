c037eb8889dac498ac703c44570a3a85272eaad6
Max Soe <maxsoe@gmail.com>
2019-01-25 15:21:49 +0400

    Conditional Rendering 1:01:04

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,54 +1,51 @@
-import React, {
-  Component,
-  Fragment
-} from 'react';
+import React, { Component, Fragment } from "react";
 
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: 'https://picsum.photos/200',
-    tags: ['tag1', 'tag2', 'tag3']
+    imageUrl: "https://picsum.photos/200",
+    // tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
 
   styles = {
     fontSize: 16,
-    fontWeight: 'bold'
-  }
+    fontWeight: "bold"
+  };
 
   render() {
     return (
       <Fragment>
         <img src={this.state.imageUrl} alt="" />
-        <span
-          style={this.styles}
-          className={this.getBadgeClasses()}
-        >
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button style={{fontSize:20}} className="btn btn-secondary btn-sm"> Increment </button>
-        <ul>
-          {this.getListItems()}
-        </ul>
+        <button style={{ fontSize: 20 }} className="btn btn-secondary btn-sm">
+          {" "}
+          Increment{" "}
+        </button>
+        {this.renderTags()}
+        {this.state.tags.length === 0 && <p>Please create a new tag</p>}
       </Fragment>
     );
   }
 
-  getListItems() {
-    const {tags} = this.state;
-    const returnTags = tags.map(tag => <li key={tag}>{tag}</li>);
-    return returnTags;
+  renderTags() {
+    const { tags } = this.state;
+    if (tags.length === 0) return (<p>There are no tags</p>);
+    return <ul>{tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 ";
-    classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
     return classes;
   }
 
   formatCount() {
     const { count } = this.state;
     const x = <p>Zero</p>;
-    return count === 0 ? x : count
+    return count === 0 ? x : count;
   }
 }
 
