efc9e1fe05a63faa5aa964a31718ef2a2041491e
Max Soe <maxsoe@gmail.com>
2019-01-25 22:40:57 +0400

    Passing Data to Components 1:24:31

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     imageUrl: "https://picsum.photos/200",
     // tags: ["tag1", "tag2", "tag3"]
     tags: []
@@ -20,7 +20,7 @@ class Counter extends Component {
 
   handleIncrement = product => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   renderTags() {
@@ -37,33 +37,30 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value } = this.state;
     const x = <p>Zero</p>;
-    return count === 0 ? x : count;
+    return value === 0 ? x : value;
   }
 
   render() {
     return (
-      <Fragment>
-        <img src={this.state.imageUrl} alt="" />
+      <div>
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={this.handleIncrement}
           style={{ fontSize: 20 }}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.renderTags()}
-        {this.state.tags.length === 0 && <p>Please create a new tag</p>}
-      </Fragment>
+      </div>
     );
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,10 +4,10 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
+      { id: 3, value: 6 },
+      { id: 4, value: 7 }
     ]
   };
 
@@ -15,7 +15,7 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </div>
     );
