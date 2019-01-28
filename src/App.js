2879a2d1a73e4e98c089e28ca3f9f9062d6f30b7
Max Soe <maxsoe@gmail.com>
2019-01-28 12:48:56 +0400

    Lifting the State Up 2:06:18

--- src/App.js
+++ src/App.js
@@ -4,12 +4,57 @@ import Counters from "./components/counters";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 6 },
+      { id: 4, value: 7 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    // console.log("counter is ", counter);
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    console.log(this.state.counters[index]);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    console.log(" Even handler called", counterId);
+
+    const counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
+
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
       </React.Fragment>
     );
